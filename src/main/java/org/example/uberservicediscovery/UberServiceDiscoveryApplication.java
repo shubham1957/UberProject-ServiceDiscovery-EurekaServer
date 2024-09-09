d35a53923ed0896afde7a7275d20ba5db3569136
Shubham Sinha <sinhashubham371@gmail.com>
2024-09-09 22:19:53 +0530

    Initial eureka server setup

---
+++ src/main/java/org/example/uberservicediscovery/UberServiceDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package org.example.uberservicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class UberServiceDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberServiceDiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/uberservicediscovery/UberServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.uberservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
